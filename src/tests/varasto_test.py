4e16b25033f2f020eaec1d5f7eaee1a8a702b926
Joona Kauppila <joona.kauppila@helsinki.fi>
2021-11-08 19:18:56 +0200

    rikottu yksi testi

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -81,4 +81,4 @@ class TestVarasto(unittest.TestCase):
     def test_str_palauttaa_oikein(self):
         self.varasto.lisaa_varastoon(5)
 
-        self.assertEqual(str(self.varasto), "saldo = 5, vielä tilaa 5")
+        self.assertEqual(str(self.varasto), "saldo = 5, vielä tilaa 4")
