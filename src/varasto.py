b37cfc704dce94bc87756a96f337bc673a1250b2
Joona Kauppila <joona.kauppila@helsinki.fi>
2021-11-15 13:23:50 +0200

    korjattu luodut tyylivirheet

--- src/varasto.py
+++ src/varasto.py
@@ -1,34 +1,16 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        self.tilavuus = max(tilavuus, 0.0)
 
+        # täyteen ja ylimäärä hukkaan!
+        self.saldo = tilavuus
         if alku_saldo < 0.0: # virheellinen, nollataan
-            self.saldo = 0.0 
+            self.saldo = 0.0
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
-            if True:
-                if not False:
-                    a = 1
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
-        b = 2
-        c = 3
-        e = 1
-        f = 1
-        g = 1
-        h = 1
-        i = 1
-        j = 1
-        k = 1
-        l = 1
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
