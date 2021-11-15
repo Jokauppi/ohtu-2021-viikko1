4992b58c04ae588786026ae9352079884bd51b20
Joona Kauppila <joona.kauppila@helsinki.fi>
2021-11-15 13:08:25 +0200

    Lisätty pylint ja analyysin rikkovat tyylivirheet

--- src/varasto.py
+++ src/varasto.py
@@ -5,18 +5,30 @@ class Varasto:
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
+
+        if alku_saldo < 0.0: # virheellinen, nollataan
+            self.saldo = 0.0 
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
+            if True:
+                if not False:
+                    a = 1
         else:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
+        b = 2
+        c = 3
+        e = 1
+        f = 1
+        g = 1
+        h = 1
+        i = 1
+        j = 1
+        k = 1
+        l = 1
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti teksti.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
@@ -31,7 +43,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +52,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
